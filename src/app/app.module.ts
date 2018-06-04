22f7f84b23f28d8247f96954b70d984e82f803b8
marcosfernandocosta <P@$w0rd>
2018-06-04 00:08:27 -0300

    only number directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './core/app-routing.module';
 import { AppComponent } from './app.component';
+
 // LTE Modules
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
